17daaa2492cf6c6514694d4f470126a5f262f76a
gmarcha <gmarcha@github.com>
2021-07-02 12:40:02 +0200

    test readline

---
+++ readline.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+char	*line_read(char *line)
+{
+	if (line != NULL)
+	{
+		free(line);
+		line = NULL;
+	}
+	line = readline("$>");
+	if (line != NULL && *line != '\0')
+		add_history(line);
+	return (line);
+}
+
+int		main(void)
+{
+	char			*line;
+
+	while (1)
+	{
+		line = readline("$>");
+		printf("%s\n", line);
+	}
+	free(line);
+	return (0);
+}
