f798e521a7bab07be9aba8e9a99e8c337f0873b6
gmarcha <gmarcha@github.com>
2021-07-09 14:30:17 +0200

    update header: update function prototypes, update command structure

--- inc/minishell.h
+++ inc/minishell.h
@@ -2,21 +2,66 @@
 # define MINISHELL_H
 
 # include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <dirent.h>
+# include <signal.h>
+# include <errno.h>
+# include <termios.h>
+# include <sys/stat.h>
 # include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 # include "libft.h"
 
-# define BUFFER_SIZE	4096
+# define PROGRAM_NAME	"minishell"
 
-void		*ft_free_strs(char **strs);
-int			ft_ischarset(int c, const char *charset);
-size_t		ft_strclen(const char *s, const char *charset);
+typedef enum e_redirect
+{
+	NO_REDIRECTION,
+	SIMPLE_REDIRECTION,
+	DOUBLE_REDIRECTION
+}				t_redirect;
 
-int			shell_line(int fd, char **line);
+typedef struct s_cmd
+{
+	char			**args;
+	int				fd_in;
+	int				fd_out;
+	int				save_stdin;
+	int				save_stdout;
+	t_redirect		redirect_in;
+	t_redirect		redirect_out;
+	char			*name_in;
+	char			*name_out;
+	pid_t			pid_process;
+	int				exit_status;
+}					t_cmd;
 
-char		**shell_split(const char *str, const char *charset);
+
+void	close_cmd_fd(t_cmd *cmd, size_t nb_cmd);
+
+void	destroy_process(t_cmd *cmd, size_t nb_cmd, char **envp, int exit_status);
+
+void	p_error(char *name, char *errmsg, int errnum);
+
+void	execute_path(t_cmd *cmd, size_t nb_cmd, size_t index_cmd, char **envp);
+
+void	execute(t_cmd *cmd, size_t nb_cmd, size_t index_cmd, char **envp);
+
+int		launch_builtin(t_cmd *cmd, size_t nb_cmd, size_t index_cmd, char **envp);
+
+int		launch(t_cmd *cmd, size_t nb_cmd, char **envp, int exit_status);
+
+char	*line_read(char *line, char *prompt);
+
+int		redirect(t_cmd *cmd, size_t nb_cmd, size_t index_cmd, int exit_status);
+
+int		reset_redirection(t_cmd *cmd, size_t nb_cmd, size_t index_cmd);
+
+int		wait_process(t_cmd *cmd, size_t nb_cmd);
 
 #endif
