3f17f2b1d9d885352fc5192ce36a5a5819691aa1
gmarcha <gmarcha@github.com>
2021-07-15 12:04:13 +0200

    update src: update main

--- src/minishell.c
+++ src/minishell.c
@@ -36,94 +36,40 @@ int	sig_init(void)
 	return (1);
 }
 
-static char	*handle_unclosed_quotes(char *line)
+static void	minishell_loop(t_var **env, int *exit_status)
 {
-	size_t			status;
-	size_t			pos;
-	size_t			i;
+	char			*line;
+	t_cmd			*cmd;
 
-	status = 0;
-	pos = ft_strlen(line);
-	i = 0;
-	while (line[i] != '\0')
+	line = NULL;
+	line = line_read(line, PROMPT);
+	while (line != NULL)
 	{
-		if (line[i] == '\'')
+		cmd = parse_line(&line, *env, exit_status);
+		if (cmd != NULL)
 		{
-			pos = i++;
-			status = 1;
-			while (line[i] != '\0')
-			{
-				if (line[i++] == '\'')
-				{
-					status = 0;
-					break ;
-				}
-			}
-			if (status == 1)
-				break ;
+			*exit_status = launch(cmd, env, *exit_status);
+			free_cmd(cmd, cmd[0].nb_cmd);
 		}
-		else if (line[i] == '"')
-		{
-			pos = i++;
-			status = 1;
-			while (line[i] != '\0')
-			{
-				if (line[i++] == '"')
-				{
-					status = 0;
-					break ;
-				}
-			}
-			if (status == 1)
-				break ;
-		}
-		i++;
+		line = line_read(line, PROMPT);
 	}
-	if (status == 0)
-		return (ft_strdup(line));
-	return (ft_substr(line, 0, pos));
-}
-
-char	*expand_line(char *line_content)
-{
-	printf("%s\n", line_content);
-	return (line_content);
-}
-
-t_cmd	*parse_line(char *line, size_t *nb_cmd)
-{
-	char			*line_content;
-	char			*line_expand;
-
-	(void)nb_cmd;
-	line_content = handle_unclosed_quotes(line);
-	if (line_content == NULL)
-		return (NULL);
-	line_expand = expand_line(line_content);
-	exit(0);
 }
 
 int	main(int ac, char *av[], char **envp)
 {
-	t_cmd			*cmd;
-	size_t			nb_cmd;
-	char			*line;
+	t_var			*env;
 	int				exit_status;
 
-	(void)ac; (void)av;
+	(void)ac;
+	(void)av;
 	if (sig_init() == 0)
-		return (0);
+		return (EXIT_FAILURE);
+	env = construct_globals(envp);
+	if (env == NULL)
+		return (EXIT_FAILURE);
 	exit_status = 0;
-	line = NULL;
-	line = line_read(line, "$> ");
-	while (line != NULL)
-	{
-		nb_cmd = 0;
-		cmd = parse_line(line, &nb_cmd);
-		exit_status = launch(cmd, nb_cmd, envp, exit_status);
-		// free_cmd(cmd, nb_cmd);
-		line = line_read(line, "$> ");
-	}
-	ft_putstr_fd("exit\n", 1);
+	minishell_loop(&env, &exit_status);
+	ft_putstr_fd("exit\n", 2);
+	clear_list(&env);
 	return (exit_status);
 }
