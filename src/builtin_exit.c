d27bba98736afbdd30e8db3efa39491d9cbb29ae
gmarcha <gmarcha@github.com>
2021-07-15 13:01:56 +0200

    update src: update exit builtin function

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 09:42:59 by qdam              #+#    #+#             */
-/*   Updated: 2021/07/14 23:00:16 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/07/15 13:00:28 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,21 @@ static void	show_error_exit(char *wrong_num)
 	ft_putendl_fd(EXIT_ARG_ERROR, STDERR_FILENO);
 }
 
-int	mini_exit(char **args, t_var **var_list_ptr)
+int	mini_exit(char **args, t_var **var_list_ptr, int exit_status)
 {
 	unsigned char	code;
 
 	ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (!args || !(*args))
-		code = 0;
-	else if ((args[0][0] == '-' && !ft_isnum(&args[0][1]))
-		|| !ft_isnum(args[0]))
+		code = exit_status;
+	else if ((*args[0] == '-' && ft_isnum(args[0] + 1))
+		|| ft_isnum(args[0]))
+		code = (unsigned char)ft_atoi(args[0]);
+	else
 	{
 		show_error_exit(args[0]);
-		code = 255;
+		code = 2;
 	}
-	else
-		code = (unsigned char)ft_atoi(args[0]);
 	clear_list(var_list_ptr);
 	return ((int)code);
 }
